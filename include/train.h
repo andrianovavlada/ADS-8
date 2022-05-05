ca569aad9efd8f150923ca2d16053fa80a9828b4
Andrianova Vlada <99177026+andrianovavlada@users.noreply.github.com>
2022-05-05 17:40:38 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -11,6 +11,9 @@ class Train {
   };
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
   Cage *first; // точка входа в поезд (первый вагон)
+  Cage* last; // последний вагон
+  Train::Cage *create(bool light);
+ 
  public:
   Train();
   void addCage(bool light); // добавить вагон с начальным состоянием лампочки
